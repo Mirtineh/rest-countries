ce65a5757c7aa50332b9f265c6143d5948373691
Mirtineh <mirfil92@gmail.com>
2022-11-30 14:31:16 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-400">This is mirtineh</h1>
+    </>
+  );
+}
+
+export default App;
