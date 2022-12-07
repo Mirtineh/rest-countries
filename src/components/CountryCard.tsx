81844f0e0c91504f56f8ac0e5910cdecbd4fcb87
Mirtineh <mirfil92@gmail.com>
2022-12-07 17:09:11 +0300

    fix overflowing problem

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -8,17 +8,17 @@ interface CountryCardProps {
 const CountryCard: FunctionComponent<CountryCardProps> = ({ country }) => {
   return (
     <>
-      <div className="w-64 h-[340px] bg-white dark:bg-dark-blue shadow rounded-md hover:cursor-pointer">
+      <div className="max-w-[256px] h-full bg-white dark:bg-dark-blue shadow rounded-md hover:cursor-pointer">
         <span className="block w-full h-40">
           <img
             src={country.flags.png}
             alt=""
-            className="h-full w-full object-cover rounded-t-md"
+            className="h-full  object-cover rounded-t-md"
           />
         </span>
         <div className="mx-7">
           <p className="text-lg font-extrabold my-6">{country.name.common}</p>
-          <div className="flex flex-col gap-1">
+          <div className="flex flex-col gap-1 mb-12">
             <p className="text-sm">
               <span className="font-semibold">Population: </span>
               <span className="font-light">
